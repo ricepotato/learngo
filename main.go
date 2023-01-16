4bbd825c155f90e444754fc078ecc51daec1de86
Ben <ben@ahnlab.io>
2023-01-16 20:03:57 +0900

    consts, variables

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	const name string = "ricepotato"  // constants
+	var nameVar string = "ricepotato" // variables
+	nameExp := "ricepotato"           // var. only in function
+
 	fmt.Println("Hello World")
+	fmt.Println(name)
+	fmt.Println(nameVar)
+	fmt.Println(nameExp)
 }
