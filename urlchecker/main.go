4a8b359285d4ce39e79d9839e0b717f8654e3ebd
ricepotato <sukjun40@naver.com>
2023-01-21 23:50:32 +0900

    slow hit url

--- urlchecker/main.go
+++ urlchecker/main.go
@@ -4,25 +4,38 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var errRequestFailed = errors.New("request failed")
 
+// the main function doesn't wait for go routines to finish
 func main() {
 	urls := []string{
 		"https://airbnb.com",
 		"https://www.google.com",
 		"https://www.amazon.com",
 		"https://www.reddit.com",
+		"https://soundcloud.com",
 		"https://www.facebook.com",
-		"https://www.youtube.com",
-		"https://www.yahoo.com",
-		"https://www.wikipedia.org",
-		"https://www.twitter.com",
+		"https://www.instagram.com",
 	}
 
+	results := map[string]string{}
+
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	//fmt.Println(results)
+
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -30,7 +43,15 @@ func hitURL(url string) error {
 	fmt.Println(fmt.Sprint("Hitting URL: ", url))
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err)
 		return errRequestFailed
 	}
 	return nil
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
 }
